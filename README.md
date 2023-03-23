3afcefe8d2d7f586405ee45c49e27cdc0d07212f
ahong <1002363357@qq.com>
2023-03-23 18:16:30 +0800

    first version for vue2

---
+++ README.md
@@ -0,0 +1,164 @@
+# hammer-vue-plugin
+
+[Hammer.js](http://hammerjs.github.io/) 是一个可以识别 touch、mouse 和 pointerEvents 的手势库，而 hammer-vue-plugin 是基于 Hammer.js 实现的 Vue 插件，允许以 Vue 指令的方式快速添加手势监听器。
+
+
+
+## 安装
+
+```bash
+npm i hammer-vue-plugin
+```
+
+
+
+## 接口
+
+### 指令列表
+
+v-hammer
+
+- 作用：绑定事件监听器
+- 参数：事件名，不支持动态参数
+- 指令值：Function | null
+
+v-hammer-manager-options
+
+- 作用：设置 Manager 实例
+- 指令值：Object
+
+v-hammer-recognizer-options
+
+- 作用：设置 Recognizer 识别器
+- 指令值：Object | Array
+
+
+
+### 标准事件
+
+以下标准事件只使用 `v-hammer` 指令添加事件监听器即可：
+
+- tap：点击事件，一个手指放下并快速抬起
+  - v-hammer:tap 单击
+  - v-hammer:doubletap  双击
+- pan：拖动事件，一个手指放下并移动
+  - v-hammer:pan
+  - v-hammer:panstart
+  - v-hammer:panmove
+  - v-hammer:panend
+  - v-hammer:pancancel
+  - v-hammer:panleft
+  - v-hammer:panright
+  - v-hammer:panup
+  - v-hammer:pandown
+- swipe：滑动事件，一个手指放下并快速滑动
+  - v-hammer:swipe
+  - v-hammer:swipeleft
+  - v-hammer:swiperight
+  - v-hammer:swipeup
+  - v-hammer:swipedown
+- press：长按事件，一个手指放下并保持一段时间的时间
+  - v-hammer:press
+  - v-hammer:pressup
+- pinch：捏合事件，两个手指放下并缩放
+  - v-hammer:pinch
+  - v-hammer:pinchstart
+  - v-hammer:pinchmove
+  - v-hammer:pinchend
+  - v-hammer:pinchcancel
+  - v-hammer:pinchin
+  - v-hammer:pinchout
+- rotate：旋转事件，两个手指放下并旋转
+  - v-hammer:rotate
+  - v-hammer:rotatestart
+  - v-hammer:rotatemove
+  - v-hammer:rotateend
+  - v-hammer:rotatecancel
+
+
+
+## 使用
+
+### 添加/移除事件监听器
+
+```vue
+<div v-hammer:tap="onTap"></div>
+
+<!-- 通过 bind 给监听器传递参数 -->
+<div v-hammer:tap="onTap.bind(this, 'ahong')"></div>
+
+<!-- 使用 null 移除事件 -->
+<div v-hammer:tap="null"></div>
+```
+
+
+
+### Hammer Manager 配置
+
+```vue
+<div v-hammer-manager-options="{ enable: false }"></div>
+```
+
+**配置项：**
+
+- enable：Boolean
+- domEvents：Boolean
+- touchAction：String，默认 compute，可选值有 compute、auto、pan-y、pan-x、none
+- cssProps：Object，只在初始绑定时生效，无法更新（PS：Hammer.Manager.prototype.set 不支持更新此配置）
+
+> 更多配置说明见：http://hammerjs.github.io/api/
+
+
+
+### Hammer Recognizer 配置
+
+```vue
+<!-- 设置 doubletap 事件可以与 tap 事件一起被识别 -->
+<div
+	v-hammer:tap="onTap"
+	v-hammer:doubletap="onDoubleTap"
+	v-hammer-recognizer-options="{
+		event: 'doubletap', recognizeWith: 'tap'
+	}"
+></div>
+```
+
+```vue
+<!-- 设置多个识别器 -->
+<div
+	v-hammer-recognizer-options="[
+		{ type: 'tap', pointers: 2 },
+        { type: 'pan', direction: 'DIRECTION_LEFT' }
+	]"
+></div>
+```
+
+配置项：
+
+- type：String，必须，可选值有 tap、pan、swipe、press、pinch、rotate，用于确定所需设置的识别器类型
+- event：String，事件名，是 Hammer 识别手势时触发的唯一名称，不传时设置的是 type 对应的识别器
+
+> 每种识别器都有自己的配置项，具体配置见 [Tap](http://hammerjs.github.io/recognizer-tap/)、[Pan](http://hammerjs.github.io/recognizer-pan/)、[Swipe](http://hammerjs.github.io/recognizer-swipe/)、[Press](http://hammerjs.github.io/recognizer-press/)、[Pinch](http://hammerjs.github.io/recognizer-pinch/)、[Rotate](http://hammerjs.github.io/recognizer-rotate/)
+
+说明：
+
+​	doubletap 属于插件内置的自定义事件，在设置 doubletap 识别器的时候需要使用 event 指定。
+
+
+
+### 自定义事件
+
+Hammer.js 支持自定义识别器，实现更加丰富的手势。
+
+```vue
+<template>
+	<!-- 自定义三次点击的事件 -->
+	<div v-hammer:tripletap="onCustom"></div>
+	<div v-hammer-recognizer-options="{
+		type: 'tap', event: 'tripletap', taps: 3
+	}"></div>
+</template>
+```
+
+
+
---
+++ index.js
@@ -0,0 +1,172 @@
+/**
+ * Vue2自定义指令：https://v2.cn.vuejs.org/v2/guide/custom-directive.html
+ */
+function captialize(value) {
+    if (typeof value !== 'string') {
+        value = String(value);
+    }
+    return value[0].toUpperCase() + value.substring(1).toLowerCase();
+}
+function errorHandler(message) {
+    throw Error(`[HammerVuePlugin error]: ${message}`);
+}
+
+// 存储绑定的识别器配置
+const RECOGNIZER_OPTIONS_MAP = new WeakMap();
+
+// 根据事件名称获取识别器类型
+function getRecognizerTypeFromEventName(name) {
+    if (['tap', 'doubletap'].includes(name)) {
+        return 'tap';
+    } else if (['pan', 'panstart', 'panmove', 'panend', 'pancancel', 'panleft', 'panright', 'panup', 'pandown'].includes(name)) {
+        return 'pan';
+    } else if (['swipe', 'swipeleft', 'swiperight', 'swipeup', 'swipedown'].includes(name)) {
+        return 'swipe';
+    } else if (['press', 'pressup'].includes(name)) {
+        return 'press';
+    } else if (['pinch', 'pinchstart', 'pinchmove', 'pinchend', 'pinchcancel', 'pinchin', 'pinchout'].includes(name)) {
+        return 'pinch';
+    } else if (['rotate', 'rotatestart', 'rotatemove', 'rotateend', 'rotatecancel'].includes(name)) {
+        return 'rotate';
+    } else {
+        return 'custom';
+    }
+}
+
+// 根据事件名称获取识别器的默认配置
+function getRecognizerDefaultOptionsFromEventName(name) {
+    if (name === 'doubletap') {
+        return {
+            type: 'tap',
+            event: name,
+            taps: 2
+        };
+    }
+    return null;
+}
+
+/**
+ * 给 Manager 添加 recognizer 手势识别器
+ * @param {Object} manager Hammer.Manager 实例
+ * @param {Object} options recongizer 配置项
+ */
+function setupRecognizer(manager, options) {
+    let type = options.type;
+    if (!type) {
+        errorHandler('custom event requires type attribute');
+        return;
+    }
+
+    let name = options.event || options.type;
+    if (options.direction) {
+        options.direction = Hammer[options.direction];
+    }
+
+    let recognizer = manager.get(name);
+    if (recognizer) {
+        recognizer.set(options);
+    } else {
+        recognizer = manager.add(new Hammer[captialize(type)](options));
+    }
+
+    if (options.recognizeWith) {
+        recognizer.recognizeWith(options.recognizeWith);
+    }
+    if (options.dropRecognizeWith) {
+        recognizer.dropRecognizeWith(options.dropRecognizeWith);
+    }
+    if (options.requireFailure) {
+        recognizer.requireFailure(options.requireFailure);
+    }
+    if (options.dropRequireFailure) {
+        recognizer.dropRequireFailure(options.dropRequireFailure);
+    }
+}
+
+export const HammerVuePlugin = {
+    install(Vue) {
+        function recognizerOptionsHandle(el, binding) {
+            let ma = el.ma || (el.ma = new Hammer.Manager(el));
+            let value = binding.value;
+            if (!Array.isArray(value)) {
+                value = [value];
+            }
+
+            RECOGNIZER_OPTIONS_MAP.set(el, value);
+            value.forEach((options) => {
+                let name = options.event || options.type;
+                let defaultOptions = getRecognizerDefaultOptionsFromEventName(name) || {};
+                setupRecognizer(ma, Object.assign(defaultOptions, options));
+            });
+        }
+
+        Vue.directive('hammer-manager-options', {
+            // Hammer.Manager.prototype.set 方法无法设置 cssProps，此配置必须在创建实例时就设置
+            // 所以在 v-hammer 所需的钩子函数之前，需要根据配置项创建好 Manager 实例
+            bind(el, binding) {
+                if (!el.ma) {
+                    el.ma = new Hammer.Manager(el, binding.value);
+                }
+            },
+            update(el, binding) {
+                if (el.ma && binding.value) {
+                    el.ma.set(binding.value);
+                }
+            }
+        });
+
+        Vue.directive('hammer-recognizer-options', {
+            // 因为这个指令最好在 v-hammer 之后执行，所以没有使用简写的方式
+            // 指令执行顺序与绑定顺序有关，如果这个指令使用 recognizeWith 等方式涉及到其它识别器，而对应识别器又还没添加时会报错
+            inserted: recognizerOptionsHandle,
+            update: recognizerOptionsHandle
+        });
+
+        Vue.directive('hammer', {
+            inserted(el, binding) {
+                let ma = el.ma || (el.ma = new Hammer.Manager(el));
+                let { arg, value } = binding;
+
+                // 设置对应的识别器，如果是自定义事件需要通过识别器指令设置
+                let type = getRecognizerTypeFromEventName(arg);
+                if (type !== 'custom') {
+                    let options = getRecognizerDefaultOptionsFromEventName(arg) || { type };
+
+                    // 当识别器配置指令先调用时有用（即：在 template 中先绑定识别器配置指令），取绑定的配置覆盖默认配置
+                    let bindingOptions = RECOGNIZER_OPTIONS_MAP.get(el);
+                    if (bindingOptions) {
+                        Object.assign(options, bindingOptions.find((options) => (options.event || options.type) === arg));
+                    }
+
+                    setupRecognizer(ma, options);
+                }
+
+                // 添加事件监听器
+                if (typeof value === 'function') {
+                    ma.on(arg, value);
+                }
+            },
+            update(el, binding) {
+                let ma = el.ma || (el.ma = new Hammer.Manager(el));
+                let { arg, value, oldValue } = binding;
+
+                // 如果事件处理函数变了，取消旧的，绑定新的
+                if (value !== oldValue) {
+                    if (typeof oldValue === 'function') {
+                        ma.off(arg, oldValue);
+                    }
+                    if (typeof value === 'function') {
+                        ma.on(arg, value);
+                    }
+                }
+            },
+            unbind(el) {
+                if (el.ma) {
+                    el.ma.destroy();
+                    el.ma = null;
+                }
+                RECOGNIZER_OPTIONS_MAP.delete(el);
+            }
+        });
+    }
+};
